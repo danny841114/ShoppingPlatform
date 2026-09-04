082fe6f78bb9a26e67e488586423c19c54244fb6
Danny Lin <danny841114@gmail.com>
2026-09-04 01:41:00 +0000

    fix method toJson: resolve error when Instant fields exist

--- src/main/java/com/danny/shoppingplatform/util/JsonHelper.java
+++ src/main/java/com/danny/shoppingplatform/util/JsonHelper.java
@@ -2,11 +2,18 @@ package com.danny.shoppingplatform.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class JsonHelper {
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
 
     /**
      * 單純轉成 JSON 字串，方便其他地方調用
